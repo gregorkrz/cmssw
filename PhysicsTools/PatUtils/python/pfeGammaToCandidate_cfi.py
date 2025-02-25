5882d0552246dbca977e1deed21b802a451d7990
Gregor Kržmanc <gregor.krzmanc@gmail.com>
2025-02-25 11:53:30 +0100

    Update pfeGammaToCandidate_cfi.py

--- PhysicsTools/PatUtils/python/pfeGammaToCandidate_cfi.py
+++ PhysicsTools/PatUtils/python/pfeGammaToCandidate_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 pfEGammaToCandidateRemapper = cms.EDProducer("PFEGammaToCandidate",
-    electrons = cms.InputTag("gedGsfElectrons"),
-    photons = cms.InputTag("gedPhotons"),
+    electrons = cms.InputTag("electrons"),
+    photons = cms.InputTag("photons"),
     #electron2pf = cms.InputTag("particleBasedIsolation","gedGsfElectrons"), 
     #photon2pf = cms.InputTag("particleBasedIsolation","gedPhotons"), 
     #pf2pf = cms.InputTag("FILLME")
